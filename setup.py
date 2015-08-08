428ed532c04d03d162c620c38638c3f8a9e4a765
Kyle <kyle@citrine.io>
2015-08-07 17:06:05 -0700

    add url to setup

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='CitrinationClient',
       version='1.0.0',
+      url='http://github.com/CitrineInformatics/python-citrination-client',
       description='Python client for accessing the Citrination api.',
       author='Kyle Michel',
       author_email='kyle@citrine.io',
