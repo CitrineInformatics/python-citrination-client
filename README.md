0dc205ced9eafdce4d794afae9385cd8676a7ec5
kjaym <kylemichel@gmail.com>
2016-08-02 16:49:21 -0700

    update for matrix site and pypi
    update readme

--- README.md
+++ README.md
@@ -2,23 +2,17 @@
 
 ## About
 
-This package is an implementation of the [Citrination API](http://citrineinformatics.github.io/api-documentation) written in python.
+This package is an implementation of the Citrination API.
 
 ## Installation
-
-### Requirements
-
-* Python 2.7
-* Requests Python library
 
 ### Setup
 
 ```shell
-$ git clone git@github.com:CitrineInformatics/python-citrination-client.git
-$ cd python-citrination-client
-$ python setup.py install
+$ pip install citrination-client
 ```
 
-## Use
+### Legacy information
 
-A detailed discussion of the citrination API and usage are available at [http://citrineinformatics.github.io/api-documentation](http://citrineinformatics.github.io/api-documentation).
+In order to interact with older versions of Citrination, please use
+the legacy branch on this repo.
--- citrination_client/citrination_client.py
+++ citrination_client/citrination_client.py
@@ -13,86 +13,20 @@ class CitrinationClient(object):
         Constructor.
 
         :param api_key: Authentication token.
-        :type api_key: String
-
         :param site: Specific site on citrination.com to work with. By default this client interacts with
         https://citrination.com. This should point to the full url of the site to access. For example, to access
         the STEEL site on citrination, use 'https://STEEL.citrination.com'.
-        :type site: String
         """
         self.headers = {'X-API-Key': urllib.quote(api_key), 'Content-Type': 'application/json'}
         self.api_url = site+'/api'
 
-    def search(self, term=None, formula=None, property=None, contributor=None, reference=None, min_measurement=None,
-               max_measurement=None, from_record=None, per_page=None, data_set_id=None):
-        """
-        Retrieve search results from Citrination. Searches are extremely inclusive and will include any records that
-        contain one or more words found in the term argument.
-
-        :param term: General search string. This is searched against all fields.
-        :type term: String
-
-        :param formula: Filter for the chemical formula field. Only those results that have chemical formulas that
-        contain this string will be returned.
-        :type formula: String
-
-        :param property: Name of the property to search for.
-        :type property: String
-
-        :param contributor: Filter for the contributor field. Only those results that have contributors that contain
-        this string will be returned.
-        :type contributor: String
-
-        :param min_measurement: Minimum of the property value range.
-        :type min_measurement: String or number.
-
-        :param max_measurement: Maximum of the property value range.
-        :type max_measurement: String or number.
-
-        :param reference: Filter for the reference field. Only those results that have contributors that contain
-        this string will be returned.
-        :type reference: String
-
-        :param from_record: Index of the first record to return (indexed from 0).
-        :type from_record: Integer
-
-        :param per_page: Number of results to return.
-        :type per_page: Integer
-
-        :param data_set_id: Id of the particular data set to search on.
-        :type data_set_id: Integer
-
-        :return: Result of the requests.post method
-        (see http://www.python-requests.org/en/latest/user/quickstart/#response-content). If the post returns a 200
-        message, then the search results can be converted to a python list/dictionary using the .json() method on
-        the return object.
-        """
-        url = self._get_search_url(data_set_id)
-        data = {'term': term, 'formula': formula, 'property': property, 'contributor': contributor,
-                'reference': reference, 'min_measurement': min_measurement, 'max_measurement': max_measurement,
-                'from': from_record, 'per_page': per_page}
-        return requests.post(url, data=json.dumps(data), headers=self.headers)
-
-    def _get_search_url(self, data_set_id):
-        """
-        Helper method to generate the url for search.
-
-        :param data_set_id: Id of the particular data set to search on.
-        :type data_set_id: Integer
-
-        :return: String with the url for searching.
-        """
-        return self.api_url+'/mifs/search' if data_set_id is None else \
-            self.api_url+'/data_sets/'+str(data_set_id)+'/mifs/search'
-
     def upload_file(self, file_path, data_set_id):
         """
-        Upload file to Citrination
-        :param file_path: File path to upload.
-        :type file_path: String
+        Upload file to Citrination.
 
-        :param dataset: The dataset id to upload the file to.
-        :type dataset: Integer
+        :param file_path: File path to upload.
+        :param data_set_id: The dataset id to upload the file to.
+        :return: Response object or None if the file was not uploaded.
         """
         url = self._get_upload_url(data_set_id)
         r = requests.get(url, headers=self.headers)
@@ -108,16 +42,16 @@ class CitrinationClient(object):
         """
         Helper method to generate the url for file uploading.
 
-        :param data_set_id: Id of the particular data set to search on.
-        :type data_set_id: Integer
-
-        :return: String with the url for uploading directly to S3
+        :param data_set_id: Id of the particular data set to upload to.
+        :return: String with the url for uploading to Citrination.
         """
         return self.api_url+'/data_sets/'+str(data_set_id)+'/upload'
 
     def create_data_set(self):
         """
         Create a new data set.
+
+        :return: Response from the create data set request.
         """
         url = self._get_create_data_set_url()
         return requests.post(url, headers=self.headers)
@@ -125,12 +59,17 @@ class CitrinationClient(object):
     def _get_create_data_set_url(self):
         """
         Helper method to generate the url for creating a new data set.
+
+        :return: URL for creating a new data set.
         """
         return self.api_url+'/data_sets'
 
     def create_data_set_version(self, data_set_id):
         """
         Create a new data set version.
+
+        :param data_set_id: Id of the particular data set to upload to.
+        :return: Response from the create data set version request.
         """
         url = self._get_create_data_set_version_url(data_set_id)
         return requests.post(url, headers=self.headers)
@@ -138,5 +77,7 @@ class CitrinationClient(object):
     def _get_create_data_set_version_url(self, data_set_id):
         """
         Helper method to generate the url for creating a new data set version.
+
+        :return: URL for creating new data set versions.
         """
         return self.api_url+'/data_sets/'+str(data_set_id)+'/version'
