ea0c8f0df553cfa9af3d9243120790682bba5c84
Kyle <kyle@citrine.io>
2016-10-07 21:38:21 -0700

    bump pypif to 1.0.17

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='citrination-client',
-      version='1.1.10',
+      version='1.1.11',
       url='http://github.com/CitrineInformatics/python-citrination-client',
       description='Python client for accessing the Citrination api',
       packages=find_packages(),
       install_requires=[
           'requests==2.10.0',
-          'pypif==1.0.16',
+          'pypif==1.0.17',
           'six==1.10.0'
       ])
