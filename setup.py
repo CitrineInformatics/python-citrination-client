236d30c246c8c87ccea50abbf814beb3344aad34
kjaym <kylemichel@gmail.com>
2017-01-19 16:29:51 -0800

    allow for random query results

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='citrination-client',
-      version='1.2.0',
+      version='1.2.1',
       url='http://github.com/CitrineInformatics/python-citrination-client',
       description='Python client for accessing the Citrination api',
       packages=find_packages(),
