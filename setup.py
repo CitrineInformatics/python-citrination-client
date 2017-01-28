75fcda5e6a575bf4a384714b55368bbb0f24e87e
kjaym <kylemichel@gmail.com>
2017-01-27 23:40:34 -0800

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='citrination-client',
-      version='1.2.9',
+      version='1.2.10',
       url='http://github.com/CitrineInformatics/python-citrination-client',
       description='Python client for accessing the Citrination api',
       packages=find_packages(),
