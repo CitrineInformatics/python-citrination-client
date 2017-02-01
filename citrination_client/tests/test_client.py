0a5ae950069d8291c2300a4666fe26877b8ebe9c
Max Hutchinson <maxhutch@gmail.com>
2017-02-01 17:39:43 -0600

    Trying to add test

--- citrination_client/tests/test_client.py
+++ citrination_client/tests/test_client.py
@@ -6,11 +6,11 @@ from pypif.pif import dump
 from os.path import join
 
 def test_start_client():
-    client = CitrinationClient(environ['CITRINATION_API_KEY'], 'https://stage.citrination.com')
+    client = CitrinationClient(environ['CITRINATION_API_KEY'], environ['CITRINATION_SITE'])
 
 
 def test_upload_pif():
-    client = CitrinationClient(environ['CITRINATION_API_KEY'], 'https://stage.citrination.com')
+    client = CitrinationClient(environ['CITRINATION_API_KEY'], environ['CITRINATION_SITE'])
     dataset = loads(client.create_data_set(name="Tutorial dataset", description="Dataset for tutorial", share=0).content.decode('utf-8'))['id']
     pif = System()
     pif.id = 0
