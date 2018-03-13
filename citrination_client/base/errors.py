7272e2191913f7619c071f54db3f167f23efdcc9
Max Gallant <mgallant@citrine.io>
2018-03-13 14:03:52 -0700

    Improve Error Handling In Base Client
      There are a few response codes which might come from Citrination
      which have a known meaning. This commit adds custom exceptions
      to represent those codes more meaningfully to the user.

--- citrination_client/base/errors.py
+++ citrination_client/base/errors.py
@@ -1,5 +1,21 @@
-class APIVersionMismatchException(Exception):
-  pass
+from citrination_client.errors import CitrinationClientError
 
-class FeatureUnavailableException(Exception):
-  pass
+class APIVersionMismatchException(CitrinationClientError):
+
+  def __init__(self, message="Version mismatch with Citrination identified. Please check for available PyCC updates"):
+      super(APIVersionMismatchException, self).__init__(message)
+
+class FeatureUnavailableException(CitrinationClientError):
+
+  def __init__(self, message="Access to an unauthorized resource requested"):
+      super(FeatureUnavailableException, self).__init__(message)
+
+class UnauthorizedAccessException(CitrinationClientError):
+
+  def __init__(self, message="Access to an unauthorized resource requested"):
+      super(UnauthorizedAccessException, self).__init__(message)
+
+class CitrinationServerErrorException(CitrinationClientError):
+
+  def __init__(self, message=None):
+      super(CitrinationServerErrorException, self).__init__(message)
--- citrination_client/util/http.py
+++ citrination_client/util/http.py
@@ -1,7 +1,7 @@
 from citrination_client.errors import CitrinationClientError
 
-def get_success_json(response, failure_message):
-    if response.status_code != 200:
+def get_success_json(response, failure_message="Unsuccessful request to Citrination"):
+    if response.status_code >= 300:
         raise CitrinationClientError(
             "{} - Citrination returned {}".format(failure_message, response.status_code)
         )
