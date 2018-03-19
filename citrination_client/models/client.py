5fe4d392a6637c537bae3cfc4c7ef669b8207cb0
Max Gallant <mgallant@citrine.io>
2018-03-19 11:34:46 -0700

    Add Inline Docs For ModelsClient

--- citrination_client/models/client.py
+++ citrination_client/models/client.py
@@ -9,6 +9,9 @@ from .projection import Projection
 import routes
 
 class ModelsClient(BaseClient):
+    """
+    A client that encapsulates interactions with models on Citrination.
+    """
 
     def __init__(self, api_key, webserver_host="https://citrination.com"):
         members = [
@@ -18,13 +21,13 @@ class ModelsClient(BaseClient):
         ]
         super(ModelsClient, self).__init__(api_key, webserver_host, members)
 
-    def tsne(self, model_name):
+    def tsne(self, data_view_id):
         """
         Get the t-SNE projection, including z-values and labels
-        :param model_name: The model identifier (id number for data views)
-        :return: dictionary containing property names and the projection for each
+        :param data_view_id: The ID of the data view to retrieve TSNE from
+        :return: A :class:`Tsne` object representing the TSNE analysis
         """
-        analysis = self._data_analysis(model_name)
+        analysis = self._data_analysis(data_view_id)
         projections = analysis['projections']
         tsne = Tsne()
         for k, v in projections.items():
@@ -43,11 +46,11 @@ class ModelsClient(BaseClient):
         """
         Predict endpoint
 
-        :param data_view_id: The model identifier (id number for data views)
+        :param data_view_id: The ID of the data view to use for prediction
         :param candidates: A list of candidates to make predictions on
         :param method:     Method for propagating predictions through model graphs
         :param use_prior:  Whether to apply prior values implied by the property descriptors
-        :return: the response, containing a list of predicted candidates as a map {property: [value, uncertainty]}
+        :return: A :class:`PredictionResult`
         """
         body = self._get_predict_body(candidates, method, use_prior)
 
@@ -64,7 +67,7 @@ class ModelsClient(BaseClient):
 
         :param model_path: The path from the custom model url (https://citrination.com/predict/{{model_path}})
         :param candidates: A list of candidates to make predictions on
-        :return: the response, containing a list of predicted candidates as a map {property: [value, uncertainty]}
+        :return: A :class:`PredictionResult`
         """
 
         body = self._get_predict_body(candidates)
--- citrination_client/models/projection.py
+++ citrination_client/models/projection.py
@@ -1,6 +1,18 @@
 class Projection(object):
+    """
+    A projection to be included in the TSNE analysis.
+    """
 
     def __init__(self, xs, ys, zs, labels, uids):
+        """
+        Constructor.
+
+        :param xs: A list of x values of the projection.
+        :param ys: A list of y values of the projection.
+        :param zs: A list of z values of the projection.
+        :param labels: A list of labels for the projected points
+        :param uids: A list of record UIDs for the projected points
+        """
         self._xs = xs
         self._ys = ys
         self._zs = zs
