3cf18051069a92aa8611f28f6e7873173b8b813c
kjaym <kylemichel@gmail.com>
2018-03-23 14:17:39 -0700

    Bump to version 3.3.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='citrination-client',
-      version='3.3.1',
+      version='3.3.2',
       url='http://github.com/CitrineInformatics/python-citrination-client',
       description='Python client for accessing the Citrination api',
       packages=find_packages(exclude=('docs')),
