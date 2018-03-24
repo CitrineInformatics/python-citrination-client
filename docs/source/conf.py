3a3c4559a16c6f6a594dcb0323de6704a71e202c
Max Gallant <mgallant@citrine.io>
2018-03-23 17:14:07 -0700

    Document Initialization Methods

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -158,3 +158,11 @@ texinfo_documents = [
 
 
 # -- Extension configuration -------------------------------------------------
+
+def skip(app, what, name, obj, skip, options):
+    if name == "__init__":
+        return False
+    return skip
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
