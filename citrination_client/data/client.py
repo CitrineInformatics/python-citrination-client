14a9665c2dd2a30e79294611d3f6330853878a51
Max Gallant <mgallant@citrine.io>
2018-04-04 17:09:34 -0600

    Small Tweaks

--- citrination_client/data/client.py
+++ citrination_client/data/client.py
@@ -186,7 +186,7 @@ class DataClient(BaseClient):
         """
         return self.get_dataset_files(dataset_id, "^{}$".format(file_path), version_number=version)[0]
 
-    def get_pif(self, dataset_id, uid, version = None):
+    def get_pif(self, dataset_id, uid, dataset_version = None):
         """
         Retrieves a PIF from a given dataset.
 
@@ -194,16 +194,16 @@ class DataClient(BaseClient):
         :type dataset_id: int
         :param uid: The uid of the PIF to retrieve
         :type uid: str
-        :param version: The dataset version to look for the PIF in. If nothing is supplied, the latest dataset version will be searched
-        :type version: int
+        :param dataset_version: The dataset version to look for the PIF in. If nothing is supplied, the latest dataset version will be searched
+        :type dataset_version: int
         :return: A :class:`Pif` object
         :rtype: :class:`Pif`
         """
         failure_message = "An error occurred retrieving PIF {}".format(uid)
-        if version == None:
+        if dataset_version == None:
             response = self._get(routes.pif_dataset_uid(dataset_id, uid), failure_message=failure_message)
         else:
-            response = self._get(routes.pif_dataset_version_uid(dataset_id, uid, version), failure_message=failure_message)
+            response = self._get(routes.pif_dataset_version_uid(dataset_id, uid, dataset_version), failure_message=failure_message)
 
         return pif.loads(response.content.decode("utf-8"))
 
