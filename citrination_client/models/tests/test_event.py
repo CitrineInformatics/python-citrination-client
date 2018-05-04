36a7da238ae719fc655643b80ea233e59ff20e9b
Jo Hill <joanneljhill@gmail.com>
2018-05-03 20:40:59 -0700

    Clreating class to represent a process event

---
+++ citrination_client/models/tests/test_event.py
@@ -0,0 +1,17 @@
+from citrination_client.models import Event
+
+
+def test_can_initialize_event_without_subevent():
+    title = "test"
+    subtitle = "test2"
+    progress = 0.5
+
+    e = Event(
+        title=title,
+        subtitle=subtitle,
+        normalized_progress=progress
+    )
+
+    assert e.title == title
+    assert e.subtitle == subtitle
+    assert e.normalized_progress == progress
