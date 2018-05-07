deff8cee810f5c02d8a2c8b7c5b402f71fc152f6
Jo Hill <joanneljhill@gmail.com>
2018-05-07 10:49:03 -0700

    Adding basic test for get_data_view

--- citrination_client/models/tests/test_models_client.py
+++ citrination_client/models/tests/test_models_client.py
@@ -176,3 +176,15 @@ def test_can_submit_run_with_no_target():
 
     assert_run_accepted(view_id, run, client)
     kill_and_assert_killed(view_id, run, client)
+
+#@pytest.mark.skipif(environ['CITRINATION_SITE'] != "https://stage.citrination.com", reason="Data view status tests currently only supported on stage")
+def test_get_data_view():
+    """
+    Tests that get_data_view returns the summary information for a given data view
+    """
+
+    view_id = "265"
+
+    data_view = client.get_data_view(data_view_id=view_id)
+
+    assert data_view.name == "Band gap with colors"
