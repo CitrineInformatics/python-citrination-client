41264aa96dabfdff650bef1bfe1fb8fc642c130a
Max Gallant <mgallant@citrine.io>
2018-05-10 14:28:47 -0700

    Version Bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='citrination-client',
-      version='4.0.0',
+      version='4.0.1',
       url='http://github.com/CitrineInformatics/python-citrination-client',
       description='Python client for accessing the Citrination api',
       packages=find_packages(exclude=('docs')),
