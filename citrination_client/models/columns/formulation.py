60676e7ca8b4243204836b8d92daecc3c47ed52c
Jo Hill <joanneljhill@gmail.com>
2018-09-26 11:09:32 -0700

    Adding formulation column class

---
+++ citrination_client/models/columns/formulation.py
@@ -0,0 +1,9 @@
+from citrination_client.models.columns.base import BaseColumn
+
+class FormulationColumn(BaseColumn):
+    """
+    A formulation column configuration for a data view.
+    Parameterized with the basic column options.
+    """
+
+    TYPE = "Formulation"
