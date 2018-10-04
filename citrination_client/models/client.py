6c520039544206fc893350c4cc4ed8c679548cac
Jo Hill <joanneljhill@gmail.com>
2018-10-04 12:46:03 -0700

    Fixing route

--- citrination_client/models/client.py
+++ citrination_client/models/client.py
@@ -79,7 +79,7 @@ class ModelsClient(BaseClient):
         :param dataview_id: The ID of the views
         :return:
         """
-        url = '/api/data_views/{}/retrain'.format(dataview_id)
+        url = 'data_views/{}/retrain'.format(dataview_id)
         response = self._post_json(url, data={})
         if response.status_code != requests.codes.ok:
             raise RuntimeError('Retrain requested ' + str(response.status_code) + ' response: ' + str(response.message))
