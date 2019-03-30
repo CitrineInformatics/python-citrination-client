0fe867e534db8b4993981c44b02921424061ef37
Sean Paradiso <seanparadiso@gmail.com>
2019-03-30 01:04:44 -0700

    Default to threshold 1.0
    resolves #240

--- citrination_client/views/descriptors/inorganic_descriptor.py
+++ citrination_client/views/descriptors/inorganic_descriptor.py
@@ -2,7 +2,7 @@ from citrination_client.views.descriptors.descriptor import MaterialDescriptor
 
 
 class InorganicDescriptor(MaterialDescriptor):
-    def __init__(self, key, threshold):
+    def __init__(self, key, threshold=1.0):
         self.options = dict(threshold=threshold)
         super(InorganicDescriptor, self).__init__(key, "Inorganic")
 
