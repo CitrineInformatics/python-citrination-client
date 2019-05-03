8cb6e9998516938da6fc5f475e2d4db34bdd7a10
genfx999 <kevin.a.williams@hotmail.com>
2019-05-03 12:57:18 -0400

    Remove extra space

--- docs/source/code_samples/models/design.py
+++ docs/source/code_samples/models/design.py
@@ -5,7 +5,7 @@ linear_design = models_client.submit_design_run(data_view_id=data_view_id, num_c
                                                 target=Target('Property Bulk modulus', .1), constraints=[],
                                                 sampler='Default')
 # Wait for the design to finish
-print ("DesignUUID: " + linear_design.uuid)
+print("DesignUUID: " + linear_design.uuid)
 while True:
     design_status = models_client.get_design_run_status(data_view_id, linear_design.uuid)
     if design_status.status == 'Finished':
