b48525f33bd211a43bbc8ee0cb3882275cc03757
genfx999 <kevin.a.williams@hotmail.com>
2019-05-03 00:00:15 -0400

    More minor fixes, remove infinity desc max, name of search template client

--- docs/source/code_samples/views/create_view.py
+++ docs/source/code_samples/views/create_view.py
@@ -4,7 +4,7 @@ from citrination_client.views.data_view_builder import DataViewBuilder
 dv_builder = DataViewBuilder()
 dv_builder.dataset_ids(29)
 desc = RealDescriptor(u'Property Bulk modulus',
-                      '0', 'Infinity')
+                      '0', '10000')
 dv_builder.add_descriptor(desc, 'output')
 desc = AlloyCompositionDescriptor('formula', 'ALUMINUM')
 dv_builder.add_descriptor(desc, 'input')
--- docs/source/code_samples/views/descriptor_keys.py
+++ docs/source/code_samples/views/descriptor_keys.py
@@ -1,9 +1,9 @@
 from citrination_client.views.search_template.client import SearchTemplateClient
 from os import environ
 
-stc = SearchTemplateClient(environ["CITRINATION_API_KEY"], environ["CITRINATION_SITE"])
+client = SearchTemplateClient(environ["CITRINATION_API_KEY"], environ["CITRINATION_SITE"])
 
 # Get an array of descriptor keys for dataset id 29
-res = stc.get_available_columns('29')
+res = client.get_available_columns('29')
 print(res)
 # ['formula', 'Property Bulk modulus']
