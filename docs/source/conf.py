6beb26acf39175ace22781cb72acdb846ef7efd0
genfx999 <kevin.a.williams@hotmail.com>
2019-05-20 20:09:51 -0400

    Release 5.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'Citrine Informatics'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'5.0.1'
+release = u'5.1.1'
 
 
 # -- General configuration ---------------------------------------------------
