c59285974a392b9134bd28f1a424b68f0a15d0e0
Kevin Williams <kwilliams@citrine.io>
2019-06-12 13:32:30 -0400

    Fix for debug statement str conversion

--- citrination_client/data/tests/test_data_client.py
+++ citrination_client/data/tests/test_data_client.py
@@ -140,7 +140,7 @@ def test_dataset_update():
     assert response.hits[0].name == new_name
     assert response.hits[0].description == new_description
 
-    print('Deleting: ' + dataset_id)
+    print('Deleting: ' + str(dataset_id))
     client.delete_dataset(dataset_id)
 
     # Search again until dataset is not found
@@ -150,7 +150,7 @@ def test_dataset_update():
             query=DataQuery(
                 dataset=DatasetQuery(
                     id=Filter(equal=dataset.id)))))
-        print('Search response: ' + response.hits)
+        print('Search response: ' + str(response.total_num_hits))
         if response.total_num_hits == 0:
             break
         else:
