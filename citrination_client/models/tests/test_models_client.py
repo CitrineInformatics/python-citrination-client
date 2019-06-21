25251243c5e0e477e0314a771b8cb45b6c731573
Edward Kim <ekim@citrine.io>
2019-06-21 11:03:58 -0600

    fix import

--- citrination_client/models/tests/test_models_client.py
+++ citrination_client/models/tests/test_models_client.py
@@ -2,6 +2,7 @@ from citrination_client.client import CitrinationClient
 from citrination_client.models import *
 from citrination_client.models.columns import *
 from os import environ
+import os
 import pytest
 import requests_mock
 
