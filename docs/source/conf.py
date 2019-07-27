9e292a99ab2f2113f92b235e20fa9bb7ffc8ad8d
Kevin Williams <kwilliams@citrine.io>
2019-07-27 15:50:01 -0400

    Version bump, major after dropping old predict endpoints

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = u'Citrine Informatics'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'5.2.0'
+release = u'6.0.0'
 
 # -- General configuration ---------------------------------------------------
 
