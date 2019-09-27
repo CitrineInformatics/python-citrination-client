6d994aef110c5dfe25a85c237c161ddd80aa5095
Travis Ludlum <tludlum@citrine.io>
2019-09-26 17:22:55 -0700

    Add ModelReport class and ViewsClient#get_model_reports endpoint
      The ViewsClient#get_model_reports endpoint returns a list of
      ModelReport instances.
      The ModelReport class serves as a wrapper around the model report
      JSON returned by Citrination, initially providing access to
      model performance, settings, and feature importances.

---
+++ citrination_client/views/model_report.py
@@ -0,0 +1,39 @@
+from copy import deepcopy
+
+class ModelReport(object):
+    """
+    An abstraction of a model report that wraps access to various sections
+    of the report.
+    """
+
+    """
+    :param raw_report: the dict representation of model report JSON
+    :type: dict
+    """
+    def __init__(self, raw_report):
+        self._raw_report = raw_report
+
+    @property
+    def model_name(self):
+        return self._raw_report['model_name']
+
+    @property
+    def performance(self):
+        return self._raw_report['error_metrics']
+
+    @property
+    def feature_importances(self):
+        return self._raw_report['feature_importances']
+
+    @property
+    def model_settings(self):
+        return self._raw_report['model_settings']
+
+    """
+    WARNING - the dict format returned is unstable and may change over time.
+
+    :return: a copy of the raw report that backs the instance.
+    :rtype: dict
+    """
+    def to_json(self):
+        return deepcopy(self._raw_report)
