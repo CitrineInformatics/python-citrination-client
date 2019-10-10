0a35a91a462773054db18f96a8adf03b7900d441
Travis Ludlum <tludlum@citrine.io>
2019-10-10 09:52:39 -0700

    Deprecate ModelsClient#get_data_view in favor of DataViewsClient#get
      We don't need the ModelsClient method anymore; the DataViewsClient method
      returns a more desireable object, suitable for passing back into the create
      endpoint. The existance of the ModelsClient method causes confusion and has
      been marked for deprecation in Citrination.

--- citrination_client/models/client.py
+++ citrination_client/models/client.py
@@ -288,45 +288,6 @@ class ModelsClient(BaseClient):
             next_experiments=result.get("next_experiment_results")
         )
 
-    def get_data_view(self, data_view_id):
-        """
-        Retrieves a summary of information for a given data view
-            - view id
-            - name
-            - description
-            - columns
-
-        :param data_view_id: The ID number of the data view to which the
-            run belongs, as a string
-        :type data_view_id: str
-        """
-
-        url = routes.get_data_view(data_view_id)
-
-        response = self._get(url).json()
-
-        result = response["data"]["data_view"]
-
-        datasets_list = []
-        for dataset in result["datasets"]:
-            datasets_list.append(Dataset(
-                name=dataset["name"],
-                id=dataset["id"],
-                description=dataset["description"]
-            ))
-
-        columns_list = []
-        for column in result["columns"]:
-            columns_list.append(ColumnFactory.from_dict(column))
-
-        return DataView(
-            view_id=data_view_id,
-            name=result["name"],
-            description=result["description"],
-            datasets=datasets_list,
-            columns=columns_list,
-        )
-
     def kill_design_run(self, data_view_id, run_uuid):
         """
         Kills an in progress experimental design run
