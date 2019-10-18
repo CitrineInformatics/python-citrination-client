78fdc061b2812153d91f0607db624b9c62c90690
Travis Ludlum <tludlum@citrine.io>
2019-10-18 14:49:45 -0700

    Release 6.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='citrination-client',
-      version='5.4.0',
+      version='6.0.0',
       url='http://github.com/CitrineInformatics/python-citrination-client',
       description='Python client for accessing the Citrination api',
       packages=find_packages(exclude=('docs')),
