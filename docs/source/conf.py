ec0b140040488efa076c61d28e15c0d2b36a67d2
Travis Ludlum <tludlum@citrine.io>
2019-10-21 19:29:58 -0700

    Updating documentation introduction with 6.x instructions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = u'Citrine Informatics'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'6.0.0'
+release = u'6.0.1'
 
 # -- General configuration ---------------------------------------------------
 
