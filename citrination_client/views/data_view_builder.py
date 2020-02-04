cf4dc1b951d58189ab006cd9b371d59d506348c2
Travis Ludlum <tludlum@citrine.io>
2020-02-04 11:23:30 -0800

    Create BaseDataViewBuilder and AdvancedDataViewBuilder
      Refactor the re-usable components of DataViewBuilder into the
      BaseDataViewBuilder.
      Create the AdvancedDataViewBuilder for configuring views with relations.

--- citrination_client/views/data_view_builder.py
+++ citrination_client/views/data_view_builder.py
@@ -1,37 +1,15 @@
-MAX_USER_RELATIONS = 30
+from citrination_client.views.base_data_view_builder import BaseDataViewBuilder
 
-class DataViewBuilder(object):
+class DataViewBuilder(BaseDataViewBuilder):
     """
     A low dimensional interface for building data views.  Choose datasets, add descriptors and other configuration
     options, then call build() to return the configuration object needed by the data views api
     """
 
     def __init__(self):
-        self.configuration = dict(
-            builder='simple',
-            dataset_ids=[],
-            group_by=[],
-            model_type='default',
-            descriptors=[],
-            relations=[],
-            roles=dict()
+        super(DataViewBuilder, self).__init__(
+            { 'builder': 'simple', 'roles': dict() }
         )
-
-    def dataset_ids(self, dataset_ids):
-        """
-        Sets the dataset ids to use for the view
-
-        :param dataset_ids: Array of strings, one for each dataset id
-        """
-        self.configuration['dataset_ids'] = dataset_ids
-
-    def model_type(self, model_type):
-        """
-        Sets the model type for the view
-
-        :param model_type: A string of either linear (for a linear model), or default (for Citrine's default nonlinear model)
-        """
-        self.configuration['model_type'] = model_type
 
     def add_descriptor(self, descriptor, role='ignore', group_by_key=False):
         """
@@ -53,107 +31,6 @@ class DataViewBuilder(object):
         if group_by_key:
             self.configuration["group_by"].append(descriptor.key)
 
-    def add_raw_descriptor(self, descriptor):
-        """
-        Add a raw descriptor dictionary object.
-
-        :param descriptor: A descriptor as a dictionary
-        """
-        self.configuration['descriptors'].append(descriptor)
-
-    def add_relation(self, inputs, output, relation_type='lolo'):
-        """
-        Add a manual relation.  If no relations are manually added, Citrination will automatically generate
-        relations when the view is created.
-
-        :param inputs: Array of strings or a single string of descriptor key(s) for the relation inputs
-        :param output: Single string descriptor key for the relation output
-        :param relation_type: Kind of relation, currently only 'lolo' is supported
-        """
-        relation_obj = {}
-
-        if isinstance(inputs, str):
-            relation_obj['inputs'] = [inputs]
-        elif isinstance(inputs, list):
-            if len(inputs) == 0:
-                raise ValueError("Inputs list must not be empty")
-            relation_obj['inputs'] = inputs
-        else:
-            raise ValueError("Unexpected type for inputs, expecting either a string or list of strings")
-
-        if isinstance(output, str):
-            relation_obj['output'] = output
-        else:
-            raise ValueError("Unexpected type for output, expecting a string")
-
-        if relation_type != 'lolo':
-            raise ValueError("Currently, 'lolo' is the only allowed relation type")
-
-        relation_obj['type'] = relation_type
-
-        # check for duplicate
-        relation_obj['inputs'].sort()
-        existing_relations = self.configuration['relations']
-        for ele in existing_relations:
-            ele['inputs'].sort() # make sure they are sorted first
-            if ele['inputs'] == relation_obj['inputs'] and \
-                    ele['output'] == relation_obj['output']:
-                raise ValueError("This relation duplicates an existing relation")
-
-        # check limits
-        if len(existing_relations) > MAX_USER_RELATIONS:
-            raise ValueError("Maximum Relations Reached: Citrination only supports " + str(MAX_USER_RELATIONS) +
-                             " user-defined relations. Please review the existing relations")
-
-        # simplify descriptor testing
-        descriptor_set = set()
-        for desc in self.configuration['descriptors']:
-            descriptor_set.add(desc['descriptor_key'])
-
-        # check inputs exist
-        for relation_input in relation_obj['inputs']:
-            if relation_input not in descriptor_set:
-                raise ValueError("Input " + relation_input + " is not defined as a descriptor")
-            if relation_obj['inputs'].count(relation_input) > 1:
-                raise ValueError("Input " + relation_input + " is listed more than once")
-            if relation_input == output:
-                raise ValueError("Output " + output + " is also an input")
-
-        if output not in descriptor_set:
-            raise ValueError("Output " + output + " is not defined as a descriptor")
-
-        # check duplicate input
-
-        self.configuration['relations'].append(relation_obj)
-
-    def get_dagre_graph(self):
-        """
-        Function to generate a DAGRE graph based on the descriptors and relations added to the builder
-        """
-        nodes = []
-        edges = []
-
-        for desc in self.configuration['descriptors']:
-            nodes.append({
-                'label': desc['descriptor_key'],
-                'attributes': {
-                    'style': {
-                        'fill': '#ff8200',
-                        'stroke': '#453536'
-                    }
-                }
-            })
-        for relation in self.configuration['relations']:
-            for input_key in relation['inputs']:
-                edges.append({
-                    "source": input_key,
-                    "target": relation['output']
-                })
-        return {
-            "nodes": nodes,
-            "edges": edges
-        }
-
     def set_role(self, key, role):
         """
         Sets the role of a descriptor
@@ -164,12 +41,3 @@ class DataViewBuilder(object):
         :type role: str
         """
         self.configuration['roles'][key] = role
-
-    def build(self):
-        """
-        Returns a configuration object suitable for creating a data view.
-
-        :return: See __init__ method for returned dict shape
-        :rtype: :dict
-        """
-        return self.configuration
