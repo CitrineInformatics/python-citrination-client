20eebd14f67ff6603f6d4be1e3ab6f5866777880
Travis Ludlum <tludlum@citrine.io>
2020-02-11 15:16:50 -0800

    Update relation graph image and text

--- docs/source/code_samples/views/relation_graphs.py
+++ docs/source/code_samples/views/relation_graphs.py
@@ -7,17 +7,26 @@ views_client = client.data_views
 relation_graph = client.data_views.get_relation_graph(12329)
 
 print(relation_graph)
-# {'nodes': [{'label': 'formula',
-#    'description': "\n\nFeaturized to: \n-- mean of Packing density\n-- mean of Liquid range\n...",
-#    'attributes': {'style': {'fill': '#ff8200', 'stroke': '#453536'}}},
-#   {'label': 'lolo:1',
-#    'description': '',
-#    'attributes': {'style': {'fill': '#78be20', 'stroke': '#453536'}}},
-#   {'label': 'Property Bulk modulus',
-#    'description': '',
-#    'attributes': {'style': {'fill': '#ff8200', 'stroke': '#453536'}}}],
-#  'edges': [{'source': 'formula', 'target': 'lolo:1'},
-#   {'source': 'lolo:1', 'target': 'Property Bulk modulus'}]}
+# {'edges': [{'source': 'formula', 'target': 'ML Model: 1'},
+#            {'source': 'ML Model: 1', 'target': 'Property Bulk modulus'}],
+#  'nodes': [{'attributes': {'style': {'fill': '#ff8200', 'stroke': '#453536'}},
+#             'description': 'Featurized to: \n'
+#                            '-- mean of Packing density\n'
+#                            '-- mean of Liquid range\n'
+#                            '...', (truncated for documentation purposes)
+#             'label': 'formula'},
+#            {'attributes': {'style': {'fill': '#ff8200', 'stroke': '#453536'}},
+#             'description': 'Error Metrics\n'
+#                            '- Root mean squared error  (GPa) (0.0 for a '
+#                            'perfect model) = 50.427349\n'
+#                            '- Uncertainty calibration: root mean square of '
+#                            'standardized errors (1.0 is perfectly calibrated) '
+#                            '= 2.069455\n'
+#                            '...', (truncated for documentation purposes)
+#             'label': 'Property Bulk modulus'},
+#            {'attributes': {'style': {'fill': '#78be20', 'stroke': '#453536'}},
+#             'description': 'Model Type: Lolo\nSample Count: 30\n',
+#             'label': 'ML Model: 1'}]}
 
 # Uses ``dagre_py`` to create a visualization of the relation graph in jupyter.
 # See screenshot below.
