9cd8a29ae80e0f48e34911bce1e66a94139ae27d
Kevin Williams <kwilliams@citrine.io>
2020-04-08 13:33:16 -0400

    Added test for the formulation helper function

--- citrination_client/views/tests/test_data_view_builder.py
+++ citrination_client/views/tests/test_data_view_builder.py
@@ -2,6 +2,10 @@ import pytest
 
 from citrination_client.views.data_view_builder import DataViewBuilder
 from citrination_client.views.descriptors.real_descriptor import RealDescriptor
+from citrination_client.client import CitrinationClient
+from citrination_client.views.descriptors import FormulationDescriptor
+from os import environ
+
 
 def test_add_descriptor():
     builder = DataViewBuilder()
@@ -18,3 +22,50 @@ def test_add_descriptor():
     # Make sure duplicates raise an error
     with pytest.raises(ValueError) as err:
         builder.add_descriptor(RealDescriptor("band gap", lower_bound=-1, upper_bound=1), "input")
+
+
+@pytest.mark.skipif(environ['CITRINATION_SITE'] != "https://citrination.com",
+                    reason="Formulation test only supported on open citrination")
+def test_formulation_descriptor():
+    client = CitrinationClient(environ["CITRINATION_API_KEY"])
+
+    builder = DataViewBuilder()
+    builder.dataset_ids([188718])
+
+    # The new thing..
+    builder.add_formulation_descriptor(FormulationDescriptor("Formulation (idealMassPercent)"), client.data_views)
+    builder.add_descriptor(RealDescriptor("Property pigment blue", 0, 600))
+    builder.add_descriptor(RealDescriptor("Property pigment red", 0, 600))
+    builder.add_descriptor(RealDescriptor("Property pigment green", 0, 600))
+
+    builder.add_descriptor(RealDescriptor("Property formulation blue", 0, 600))
+    builder.add_descriptor(RealDescriptor("Property formulation red", 0, 600))
+    builder.add_descriptor(RealDescriptor("Property formulation green", 0, 600))
+
+    config = builder.build()
+
+    assert config["roles"]["Formulation (idealMassPercent)"] == "input"
+    assert config["roles"]["component type"] == "ignore"
+    assert config["roles"]["name"] == "ignore"
+    assert config["roles"]["% P1 (mass, ideal)"] == "ignore"
+
+    p1_share_present = False
+    name_present = False
+    pigment_blue_present = False
+
+    print(config["descriptors"])
+
+    for desc in config["descriptors"]:
+        if desc["descriptor_key"] == "% P1 (mass, ideal)":
+            p1_share_present = True
+        if desc["descriptor_key"] == "name":
+            name_present = True
+        if desc["descriptor_key"] == "Property pigment blue":
+            pigment_blue_present = True
+
+    assert p1_share_present
+    assert name_present
+    assert pigment_blue_present
+
+
+test_formulation_descriptor()
